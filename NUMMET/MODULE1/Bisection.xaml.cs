049d9d5c61d4645feeb9e0c11a67aaf70ed2abcf
KurtSarmiento <sarmijustine@gmail.com>
2025-04-29 14:29:39 +0800

    created bisection window

--- NUMMET/MODULE1/Bisection.xaml.cs
+++ NUMMET/MODULE1/Bisection.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,36 +12,20 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace NUMMET
+namespace NUMMET.MODULE1
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class Bisection : Page
     {
-        public MainWindow()
+        public Bisection()
         {
             this.InitializeComponent();
-        }
-
-        private void Button_Bisection_Click(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void Button_NewtonRaphson_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Secant_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
